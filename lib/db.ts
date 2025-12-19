a2ebc443d1552a91cae32e2f3aa29e8255c02152
Aditya <adityapratapgoswami07@gmail.com>
2025-12-19 23:11:42 +0530

    chore: update service worker precache list with latest build assets.

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
+
 
 interface MongooseCache {
   conn: typeof mongoose | null;
@@ -26,6 +22,12 @@ if (!cached) {
 async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    );
   }
 
   if (!cached.promise) {
